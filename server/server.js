173053c4be3e40073b11abd4fae22a3d83ff2ab8
Michael <johnmichaelflores777@gmail.com>
2022-04-20 01:31:17 +0800

    Update server config

--- server/server.js
+++ server/server.js
@@ -3,17 +3,23 @@ const app = express();
 const cors = require('./middleware/cors');
 const dotenv = require('dotenv');
 const path = require('path');
-
-//* Server port 
-const PORT = process.env.PORT || 7771
+const connectDatabase = require('./config/db')
 
 //* Initialize dotenv
 dotenv.config()
 
+//* Server port 
+const PORT = process.env.PORT || 7771
+
+//* Connect to MongoDB
+connectDatabase()
+
 //* Middleware
 app.use(cors)
+app.use(express.json())
 
 //* API routes
+app.use('/api/users', require(path.join(__dirname, 'routes', 'api/users')))
 app.use('/api/banks', require(path.join(__dirname, 'routes', 'api/banks')))
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
