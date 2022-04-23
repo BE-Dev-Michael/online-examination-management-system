ad50f77e33562d13cc81028b9e0a2a7063b45df1
Erish Rey <erishs15@gmail.com>
2022-04-23 21:12:12 +0800

    Update navbar and add images folder

--- client/src/components/Landing/DropdownMenu.jsx
+++ client/src/components/Landing/DropdownMenu.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './navbar-style.css';
 
 function DropdownMenu(props) {
   return (
     <div>
      <div className={props.collapse ? '' : 'hidden'}>
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                Home
-            </a>
-            <a className="text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                Gallery
-            </a>
-            <a className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                Content
-            </a>
-            <a className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
-                Contact
-            </a>
-        </div>
+            <div className={"nav-menu active md:hidden"}>
+                <a>
+                    <Link className="nav-links-mobile" to="/">
+                        Home
+                    </Link>
+                </a>
+                <a>
+                    <Link className="nav-links-mobile" to="/#">
+                        About
+                    </Link>
+                </a>
+                <a>
+                    <Link className="nav-links-mobile" to="/login">
+                        Log In
+                    </Link>
+                </a>
+                    <Link className="sign_up-btn dropdown" to="/signup">
+                        Sign Up
+                    </Link>
+            </div>
+       
       </div>
     </div>
   )
