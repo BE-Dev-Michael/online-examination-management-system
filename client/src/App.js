d6543c13c125a95fc11179f7a388ef1524b7c507
Michael <johnmichaelflores777@gmail.com>
2022-04-23 11:01:43 +0800

    Add route for faculty dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import Landing from "./pages/Landing"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
-import { Routes, Route} from 'react-router-dom';
+import Faculty from "./pages/Faculty"
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<SignUp/>} />
+      <Route path="/faculty/*" element={<Faculty/>}/>
     </Routes>
   );
 }
