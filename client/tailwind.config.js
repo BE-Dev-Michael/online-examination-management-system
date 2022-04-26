43e720b294319459d71c8e4245cc3e11ae7515fd
Michael <johnmichaelflores777@gmail.com>
2022-04-26 17:26:55 +0800

    Update tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile' : {'max': '639px'}
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
