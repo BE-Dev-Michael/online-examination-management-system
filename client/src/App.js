38504e815d719763c9d772e1793f44c2964ef31d
Michael <johnmichaelflores777@gmail.com>
2022-04-26 17:24:14 +0800

    Update app component

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import Faculty from "./pages/Faculty"
 import { Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil'
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/signup" element={<SignUp/>} />
-      <Route path="/faculty/*" element={<Faculty/>}/>
-    </Routes>
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/faculty/*" element={<Faculty/>}/>
+      </Routes>
+    </RecoilRoot>
   );
 }
 
