def3de55bbd7a7df34b61d453900a17200df9fd8
Michael <johnmichaelflores777@gmail.com>
2022-04-26 17:07:08 +0800

    Update bg component in faculty

--- client/src/components/Faculty/Background.jsx
+++ client/src/components/Faculty/Background.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Background({children}) {
   return (
-    <div className='bank-page relative w-screen h-screen p-1 overflow-hidden'>
-      <div className='flex justify-between'>
+    <div className='bank-page relative w-screen h-screen p-5 overflow-hidden'>
+      <div className='flex gap-4'>
         {children}
       </div>
     </div>
