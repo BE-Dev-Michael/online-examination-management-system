8354fe9466c597c545d3e49c45d991b1fb7f3a16
Romeo Gatchalian <romeogatchalian24@gmail.com>
2022-04-28 14:58:08 +0800

    Add useState

--- client/src/components/Student/SideBarProfile.jsx
+++ client/src/components/Student/SideBarProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { MdDashboard } from 'react-icons/md'
 import { RiFileList2Line } from 'react-icons/ri'
