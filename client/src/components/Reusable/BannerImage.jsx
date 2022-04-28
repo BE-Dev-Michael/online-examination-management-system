84d05048bd5ebdd191b94df7c75de5ef51f384e4
Erish Rey <erishs15@gmail.com>
2022-04-29 05:43:01 +0800

    Modified img className

--- client/src/components/Reusable/BannerImage.jsx
+++ client/src/components/Reusable/BannerImage.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function BannerImage(props) {
   return (
     <div>
-      <img src={props.src} alt="" className="absolute h-full w-full object-cover"/>
-      <div className="inset-0 bg-black opacity-25 absolute"></div>
+      <img src={props.src} alt="" className="absolute lg:h-screen sm:h-max w-full object-cover"/>
+      {/* <div className="inset-0 bg-black opacity-25 absolute"></div> */}
     </div>
   )
 }
