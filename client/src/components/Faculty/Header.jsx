15c2968105df6932ff952f03adc9b013af6c4fc5
Michael <johnmichaelflores777@gmail.com>
2022-05-03 20:07:42 +0800

    Update faculty header bg color

--- client/src/components/Faculty/Header.jsx
+++ client/src/components/Faculty/Header.jsx
@@ -13,7 +13,7 @@ const profileMenuState = atom({
 function ProfileMenu() {
   return(
     <>
-      <div class="absolute right-6 -bottom-24 z-10 bg-white divide-y divide-gray-100 shadow-lg w-44 ring-2 ring-black ring-opacity-10">
+      <div class="absolute right-12 -bottom-24 z-10 bg-white divide-y divide-gray-100 shadow-lg w-44 ring-2 ring-black ring-opacity-10">
           <ul class="py-1 text-md text-gray-700" aria-labelledby="dropdownDividerButton">
             <li>
               <a href="#" class="flex items-center block px-4 py-2 hover:bg-gray-100 font-semibold">
@@ -54,17 +54,17 @@ function Header() {
  }, [isDropdownVisible])
 
   return(
-    <div className="relative w-full bg-white px-5 py-3 rounded-3xl">
+    <div className="relative w-full sm:bg-white md:bg-transparent px-5 py-3 rounded-3xl">
       <div className={`flex justify-between sm:justify-between md:justify-end`}>
         <button className="sm:block md:hidden" onClick={() => setShowSidebar(!showSidebar)}>
           <CgMenuLeft className='text-3xl cursor-pointer hover:text-[#7B9EBE] transition-all ease-linear'/>
         </button>
         <a ref={profileRef} onClick={() => setIsDropdownVisible(!isDropdownVisible)} href="#" className="block relative">
-          <img alt="profil" src="https://avatarfiles.alphacoders.com/275/275525.jpg" className="mx-auto object-cover rounded-full h-10 w-10 "/>
+          <img alt="profil" src="https://avatarfiles.alphacoders.com/275/275525.jpg" className="mr-7 object-cover rounded-full h-10 w-10 "/>
         </a>
       </div>
       {isDropdownVisible ? 
-        <div className='absolute z-20 right-8 w-0 h-0 border-l-[10px] border-l-transparent border-r-transparent border-r-[10px] border-b-[15px] border-b-[#7B9EBE]'></div>
+        <div className='absolute z-20 right-14 w-0 h-0 border-l-[10px] border-l-transparent border-r-transparent border-r-[10px] border-b-[15px] border-b-[#7B9EBE]'></div>
       : ''}
       {isDropdownVisible ? <ProfileMenu/> : ''}
     </div>
