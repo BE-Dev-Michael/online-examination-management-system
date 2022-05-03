a691e5749403ae2d88b6c7b49cac7e4ceabb4382
Michael <johnmichaelflores777@gmail.com>
2022-05-03 10:21:15 +0800

    Update question banks component

--- client/src/components/Faculty/QuestionBanks.jsx
+++ client/src/components/Faculty/QuestionBanks.jsx
@@ -5,8 +5,18 @@ import { IoAddCircleOutline, IoSearchOutline, IoClose, IoEllipsisHorizontal } fr
 import { IoMdArchive } from 'react-icons/io'
 import { FiEdit3 } from 'react-icons/fi'
 import { atom, useRecoilState, useRecoilValue } from 'recoil'
+import { useNavigate } from 'react-router-dom'
+import bankDataState from './bankDataAtom'
 
+//* GET and POST method
+//* POST for adding question bank
+//* GET for getting all question banks
 const BANKS_URI = 'http://localhost:7771/api/banks'
+
+//* GET, PATCH, and POST
+//* GET for getting questions inside a bank
+//* PATCH for updating bank title
+//* POST for adding questions in a bank
 const BANK_URI = 'http://localhost:7771/api/banks/'
 
 const banksState = atom({
@@ -38,6 +48,9 @@ const selectedBankState = atom({
   default: ''
 })
 
+
+
+
 function ActionMenu(props) {
   const [isEditMode, setIsEditMode] = useRecoilState(editModeState)
   const [isEllipsisClicked, setIsEllipsisClicked] = useRecoilState(ellipsisMenuState)
@@ -52,7 +65,7 @@ function ActionMenu(props) {
   return (
     <div className="absolute right-0 z-10 mt-2 w-44 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
       <div className="py-1 " role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-        <a ref={props.editTitle} onClick={editBankTitle} className="flex items-center block cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
+        <a ref={props.editTitle} onClick={editBankTitle} className="edit-title flex items-center block cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
             <span className="flex justify-center items-center">
                <FiEdit3 className='mr-4'/>
                 <span>
@@ -79,22 +92,21 @@ function Bank(props) {
   const isEditMode = useRecoilValue(editModeState)
   const btnThreeDots = useRef(null)
   const editTitle = useRef(null)
-
-  // useEffect(() => {
-  //     console.log(selectedBankId)
-  // }, [selectedBankId])
+  
 
   const outsideClick = (e) => {
-     if (btnThreeDots.current.contains(e.target) || editTitle.current.contains(e.target)) {
-        return
-     }
-
-     setIsEllipsisClicked(false)
+    if (btnThreeDots.current.contains(e.target) || editTitle.current.contains(e.target)) {
+      return
+    }
+    
+    setIsEllipsisClicked(false)
   }
 
   useEffect(() => {
-      document.addEventListener('mousedown', outsideClick)
-      console.log('mousedown')
+      if (isEllipsisClicked) {
+        document.addEventListener('mousedown', outsideClick)
+      }
+      
       return () => {
         document.removeEventListener('mousedown', outsideClick);
       }
@@ -110,7 +122,7 @@ function Bank(props) {
       <div className="flex flex-col items-center justify-between shadow-lg rounded-[1.2rem] w-[350px] md:w-full lg:w-full p-4 bg-white relative min-h-[240px] max-h-[240px]">
         <div className='flex justify-between items-center w-full'>
           <span className='text-gray-600 bg-[#C8CFCF] rounded-xl py-1 px-5 text-xs'>
-            {props.noOfQuestions} Questions
+            {props.noOfQuestions === 1 ? `${props.noOfQuestions} Question` : `${props.noOfQuestions} Questions`} 
           </span>
           <div className='relative'>
             <button ref={btnThreeDots} onClick={() => ellipsisHandler(props._id)}>
@@ -149,7 +161,7 @@ function EditBankMode() {
         const newTitle = await axios.patch(BANK_URI.concat(selectedBankId), { title: editTitle })
         console.log(newTitle.data._id);
 
-        //* Naguupdate ng array of objects
+        //* Naguupdate ng key-value pairs in an object inside an array
         const updatedBank = banks.map(bank => {
           if (bank.id === newTitle.data._id) {
               return {...bank, title: newTitle.data.title}
@@ -218,8 +230,9 @@ function CreateBankMode() {
         console.log(bank.data)
         setIsCreateMode(false)
         setAddTitle('')
-        //* Nagaappend ng new object sa array of objects
-        setBanks(prevState => [...prevState, {id: bank.data._id, title: bank.data.title}])
+        // //* Nagaappend ng new object sa array of objects
+        // setBanks(prevState => [...prevState, {id: bank.data._id, title: bank.data.title}])
+        window.location.reload(false);
         
       } catch (error) {
          console.error(error)
@@ -253,18 +266,23 @@ function CreateBankMode() {
 }
 
 function QuestionBanks() {
+  //* Dito maiistore lahat ng question banks
+  //* Type: Array of Objects
   const [banks, setBanks] = useRecoilState(banksState)
   const isCreateMode = useRecoilValue(createModeState)
+  const navigate = useNavigate()
+  const [, setBankData] = useRecoilState(bankDataState)
   
+  //* Get all question banks on initial render
   useEffect(() => {
     const getBanks = async () => {
         try {
             const banks = await axios.get(BANKS_URI)
             
             let fetchedBanks = banks.data.map(data => {
-                return { id: data._id, title: data.title }
+                return { id: data._id, title: data.title, questions: data.questions }
             })
-           
+            
             setBanks(fetchedBanks)
         } catch (error) {
             throw new Error(error)
@@ -273,20 +291,20 @@ function QuestionBanks() {
     getBanks()
   }, [])
 
-  useEffect(() => {
-    console.log(banks, 'Update bank!')
-  }, [banks])
-
   const openBank = async (id) => {
       try {
           const bank = await axios.get(BANK_URI.concat(id))
-          console.log(bank.data)
+          
+          setBankData(bank.data)
+          
+          navigate(`/faculty/banks/${bank.data._id}`)
       } catch (error) {
           throw new Error(error)
       }
+      
   }
   return (
-    <>
+    <div className='flex flex-col gap-4'>
       <div className='w-full px-5'>
         <h1 className='text-white text-2xl font-bold'>Question Banks</h1>
       </div>
@@ -301,15 +319,14 @@ function QuestionBanks() {
         </div>
       </div>
      
-      <div className="relative w-full h-full p-5 mb-2 overflow-y-scroll">
-        <div class="grid grid-rows-2 gap-10 md:grid-cols-2 lg:grid-cols-3 justify-center items-center ease-in-out duration-300">
+      <div className="relative w-full h-full p-5 mb-24">
+        <div className="grid grid-rows-2 gap-10 md:grid-cols-2 lg:grid-cols-3 justify-center items-center ease-in-out duration-300">
         {banks.map(bank => {
-            return <Bank _id={bank.id} open={() => openBank(bank.id)} key={bank.id} title={bank.title} noOfQuestions='17'/>
+            return <Bank _id={bank.id} open={() => openBank(bank.id)} key={bank.id} title={bank.title} noOfQuestions={bank.questions.length}/>
         })}
         </div>
       </div> 
-      
-    </>
+    </div>
   )
 }
 
