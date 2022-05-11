be2b2082672a1a0dc86435a0e52d7ae23ce332c8
Romeo Gatchalian <romeogatchalian24@gmail.com>
2022-05-11 22:51:22 +0800

    Add useEffect and fetch in examboard

--- client/src/components/Student/Examboard.jsx
+++ client/src/components/Student/Examboard.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import InputCode from './InputCode'
 import ExamCard from './ExamCard'
+import axios from 'axios'
 
 const Examboard = () => {
+    const [exams, setExams] = useState([])
+    const [examCode, setExamCode] = useState("")
+
+    useEffect(() => {
+        const fetchExams = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/examination");
+                setExams(response.data)
+            } catch (error) {
+                console.log(error.response.data)
+            }
+        }
+        fetchExams()
+    }, [])
+
+    const getExamCodeHandler = (code) => {
+        setExamCode(code)
+    }
+
     return (
         <div className="lg:flex lg:flex-row-reverse flex-col justify-center  h-full w-screen mx-5">
-            <InputCode />
+            <InputCode getCodeFunction={getExamCodeHandler} />
 
             <div className="relative w-full shadow-sm border bg-white rounded-2xl">
                 <p className="ml-9 mt-4 text-xl font-bold text-gray-500">Available Exams</p>
@@ -14,6 +34,11 @@ const Examboard = () => {
                     <ExamCard />
                     <ExamCard />
                     <ExamCard />
+
+                    {/* {exams.filter(exam => exam.examCode === examCode).map((exam, index) => {
+                        return <ExamCard key={index} id={exam.id} title={exam.title} instruction={exam.instruction} time={exam.limit} question={exam.questions} />
+                    })} */}
+
                 </div>
             </div>
         </div>
