ff63a8f80631d067a04febebea000d744ea8ad35
Romeo Gatchalian <romeogatchalian24@gmail.com>
2022-05-11 23:31:39 +0800

    Add placeholder

--- client/src/components/Login/LoginItems.jsx
+++ client/src/components/Login/LoginItems.jsx
@@ -38,7 +38,9 @@ function LoginItems() {
                 onChange={formDataHandler}
                 name="email"
                 type="text"
-                className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-slate-600 focus:border-transparent" />
+                className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-slate-600 focus:border-transparent"
+                placeholder="Enter your email" />
+
             </div>
           </div>
 
@@ -49,7 +51,8 @@ function LoginItems() {
                 onChange={formDataHandler}
                 name="password"
                 type={(showEye === false) ? 'password' : 'text'}
-                className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-slate-600 focus:border-transparent" />
+                className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-slate-600 focus:border-transparent"
+                placeholder="Enter your password" />
             </div>
             <div className='text-2xl absolute top-9 right-2'>
               {(showEye === false) ?
