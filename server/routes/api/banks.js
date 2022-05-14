90c5af962fb58baacb4321e039d472aa4aa2a11c
Michael <johnmichaelflores777@gmail.com>
2022-05-14 10:43:52 +0800

    Update banks API routes

--- server/routes/api/banks.js
+++ server/routes/api/banks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllQuestionBanks, getQuestions, addQuestion, addQuestionBank, updateQuestionBank, deleteQuestionBank} = require('../../controllers/banks.controller')
 
-router.get('/', getAllQuestionBanks)
+router.get('/all/:user', getAllQuestionBanks)
 router.get('/:id', getQuestions)
 router.post('/question/:id', addQuestion)
 router.post('/', addQuestionBank)
