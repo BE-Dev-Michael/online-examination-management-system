5d7a434257a298159ed48015da610c6ba71c8721
Michael <johnmichaelflores777@gmail.com>
2022-05-14 19:16:39 +0800

    Add reports relationship with user

--- server/routes/api/reports.js
+++ server/routes/api/reports.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getExamAndQuestions } = require('../../controllers/reports.controller')
 
-router.get('/', getExamAndQuestions)
+router.get('/:user', getExamAndQuestions)
 
 module.exports = router
