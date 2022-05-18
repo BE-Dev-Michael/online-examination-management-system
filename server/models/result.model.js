39a16d5b361c9c2b33d5acf31f18f0a41c951efe
Michael <johnmichaelflores777@gmail.com>
2022-05-18 17:44:18 +0800

    Modify exam result model

--- server/models/result.model.js
+++ server/models/result.model.js
@@ -4,14 +4,12 @@ const examResultSchema = mongoose.Schema({
     score: Number,
     remarks: String,
     completedDate: String,
+    answers: [],
+    correctAnswers: [],
     exam: [{ //* Exam result relationship with exam
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Exams'
-    }],
-    student: { //* Exam result relationship with student
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Users'
-    }
+    }]
 })
 
 module.exports = mongoose.model('Result', examResultSchema)
